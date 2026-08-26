2bc966581fef0d08ff260f8ba162e70c4ebce846
sabzyhacks <ei711283@gmail.com>
2026-08-26 20:13:46 +0100

    selecting each tab

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 "use strict";
-const tab = document.querySelectorAll(".tab");
+const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab-content");
+const currentTab = 1;
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", function (e) {
+    const isActive = tab.classList.contains("active");
+    console.log(isActive);
+  });
+});
