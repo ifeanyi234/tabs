5fc8519eb4a1d90084b8a1d281bb3d6322a462e2
sabzyhacks <ei711283@gmail.com>
2026-08-26 22:44:06 +0100

    cleaned up codebase

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
 "use strict";
 const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab-content");
-let currentTab = 1;
 
 tabs.forEach((tab) => {
-  tab.addEventListener("click", function (e) {
-    const isActive = tab.classList.contains("active");
-    currentTab = e.target.dataset.tab;
-
-    if (!isActive) {
-      tabs.forEach((tab) => {
-        tab.classList.remove("active");
-        tab.setAttribute("aria-selected", "false");
-      });
-    }
+  tab.addEventListener("click", function () {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+      tab.setAttribute("aria-selected", "false");
+    });
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
 
     tab.classList.add("active");
     tab.setAttribute("aria-selected", "true");
-    console.log(currentTab);
 
-    let currentTabElement = document.getElementById(`content-${currentTab}`);
-    console.log(currentTabElement);
-
-    if (!isActive) {
-      tabContents.forEach((tabContent) =>
-        tabContent.classList.remove("active"),
-      );
-    }
-
+    const currentTabElement = document.getElementById(
+      `content-${tab.dataset.tab}`,
+    );
     currentTabElement.classList.add("active");
   });
 });
