61b170efa10c091f9d7b8fc6572d69d05651eaf3
sabzyhacks <ei711283@gmail.com>
2026-08-26 21:16:53 +0100

    selection of current element added

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 "use strict";
 const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab-content");
-const currentTab = 1;
+let currentTab = 1;
 
 tabs.forEach((tab) => {
   tab.addEventListener("click", function (e) {
     const isActive = tab.classList.contains("active");
+    currentTab = e.target.dataset.tab;
 
     if (!isActive) {
-      tabs.forEach((tab) => tab.classList.remove("active"));
+      tabs.forEach((tab) => {
+        tab.classList.remove("active");
+        tab.setAttribute("aria-selected", "false");
+      });
     }
+
     tab.classList.add("active");
+    tab.setAttribute("aria-selected", "true");
+    console.log(currentTab);
+
+    let currentTabElement = document.getElementById(`content-${currentTab}`);
+    console.log(currentTabElement);
   });
 });
